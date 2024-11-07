271087dec2fb143851b2f790ed6a5276a31c7987
Samuel Reyna González <118919573+Meiuwu2001@users.noreply.github.com>
2024-11-06 19:25:06 -0600

    cambios server

--- functions/server.js
+++ functions/server.js
@@ -1,8 +1,8 @@
 import app from "../src/app.js";
 import serverless from "serverless-http";
 
+app.use("/.netlify/functions/server", router);
+
 const handler = serverless(app);
 
 export { handler };
-
-
--- src/routes/clientes.js
+++ src/routes/clientes.js
@@ -1,13 +1,18 @@
-import express from  "express";
-import {getCliente, createCliente, getClienteById, updateCliente, deleteCliente} from "../controllers/clientes.js";
+import express from "express";
+import {
+  getCliente,
+  createCliente,
+  getClienteById,
+  updateCliente,
+  deleteCliente,
+} from "../controllers/clientes.js";
 
 const router = express.Router();
 
-router.get("/clientes", getCliente)
-router.post("/clientes", createCliente)
-router.get("/clientes/:id", getClienteById)
-router.put("/clientes/:id", updateCliente)
-router.delete("/clientes/:id", deleteCliente)
+router.get("/clientes", getCliente);
+router.post("/clientes", createCliente);
+router.get("/clientes/:id", getClienteById);
+router.put("/clientes/:id", updateCliente);
+router.delete("/clientes/:id", deleteCliente);
 
-
-export default router;  // Exportación por defecto
+export default router; // Exportación por defecto
