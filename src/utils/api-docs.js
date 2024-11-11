d7f23337be78e2c39817a16f5789ae4d76cbd598
Samuel Reyna González <118919573+Meiuwu2001@users.noreply.github.com>
2024-11-11 11:58:05 -0600

    expor default swagger

--- src/utils/api-docs.js
+++ src/utils/api-docs.js
@@ -1,3 +1,5 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
 const swaggerOptions = {
   swaggerDefinition: {
     openapi: "3.0.0",
@@ -15,5 +17,10 @@ const swaggerOptions = {
     ],
     schemes: ["https", "http"],
   },
-  apis: ["src/routes/*.js"],
+  apis: ["src/routes/*.js"], // Archivos donde están definidas las rutas de la API
 };
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+// Cambia la exportación de default a una exportación con nombre
+export { swaggerSpec };
