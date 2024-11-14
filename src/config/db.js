eefac7951457c1b1773190b5756cdfd3c4de5d5c
Samuel Reyna González <118919573+Meiuwu2001@users.noreply.github.com>
2024-11-14 00:03:05 -0600

    a

--- src/config/db.js
+++ src/config/db.js
@@ -4,19 +4,17 @@ import dotenv from "dotenv";
 // Cargar variables de entorno
 dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  waitForConnections: true, // Espera por una conexión libre
-  connectionLimit: 10, // Número máximo de conexiones simultáneas
-  queueLimit: 0, // Sin límite de solicitudes en espera
-});
-
-const connect = () => {
-  return pool; // Retorna el pool de conexiones
+const connect = async () => {
+  return await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true, // Espera por una conexión libre
+    connectionLimit: 100, // Número máximo de conexiones simultáneas
+    queueLimit: 0, // Sin límite de solicitudes en espera
+  });
 };
 
 export default connect;
