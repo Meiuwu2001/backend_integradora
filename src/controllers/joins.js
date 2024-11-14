7c7e9d4b6ad74ef8b1c58748b0a889d8a19c987c
pao0803 <paolaherreraortiz08@gmail.com>
2024-11-14 14:31:27 -0600

    TecnicosUsuarios

--- src/controllers/joins.js
+++ src/controllers/joins.js
@@ -13,6 +13,20 @@ export const getClienteUsuario = async (req, res) =>{
    }
 }
 
+export const getTecnicosUsuarios = async (req, res)=>{
+    try{
+        const db = await connect(); 
+        const [result] = await db.query("SELECT u.user, CONCAT(t.Nombre, ' ', t.ApellidoPa) AS Tecnico, t.estatus FROM users  INNER JOIN tecnicos t ON u.idusers = t.users_idusers")
+        res.json(result);
+        
+
+    }catch{(error)
+        console.error(error);
+        res.status(500).send("Server Error")
+        await db.end();
+    }
+}
+
 
 
 //a 
--- src/routes/joins.js
+++ src/routes/joins.js
@@ -1,10 +1,12 @@
 import express from "express";
 import {
-    getClienteUsuario 
+    getClienteUsuario,
+    getTecnicosUsuarios
 } from "../controllers/joins.js"
 
 const router = express.Router();
 
 router.get("/clienteusuarios", getClienteUsuario);
+router.get("/tecnicosusuarios", getTecnicosUsuarios)
 
 export default router;
