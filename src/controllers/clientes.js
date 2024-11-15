8654caeaaafac3820795550b69d60b42536efb6e
Samuel Reyna González <118919573+Meiuwu2001@users.noreply.github.com>
2024-11-15 15:32:40 -0600

    clientes cambios

--- src/controllers/clientes.js
+++ src/controllers/clientes.js
@@ -1,75 +1,80 @@
 import connect from "../config/db.js"; // Importamos la conexión
 
-export const getCliente = async (req, res) => {
+export const getClientes = async (req, res) => {
+  let db;
   try {
-    const db = await connect();
+    db = await connect();
     const [result] = await db.query("SELECT * FROM clientes;");
     res.json(result);
-    await db.end(); 
   } catch (error) {
     console.error(error);
-    res.status(500).send("Server Error");
-    await db.end(); 
+    res.status(500).json({ error: "An error occurred while retrieving data" });
+  } finally {
+    if (db) await db.end(); // Asegúrate de cerrar la conexión siempre
   }
 };
 
 export const createCliente = async (req, res) => {
+  let db;
   try {
-    const db = await connect();
+    db = await connect();
     await db.query("INSERT INTO clientes SET ?", [req.body]);
     res.json({ status: "ok" });
-    await db.end();
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "An error occurred while creating data" });
-    await db.end();
+  } finally {
+    if (db) await db.end(); // Asegúrate de cerrar la conexión siempre
   }
 };
 
 export const getClienteById = async (req, res) => {
+  let db;
   try {
-    const db = await connect();
+    db = await connect();
     const [result] = await db.query(
       "SELECT * FROM clientes WHERE idClientes =?",
       [req.params.id]
     );
     if (!result.length) {
-      return res.status(404).json({ error: "Contact not found" });
+      return res.status(404).json({ error: "Cliente not found" });
     }
     res.json(result[0]);
-    await db.end();
   } catch (error) {
     console.error(error);
-    res.status(500).send("Server Error");
-    await db.end(); 
+    res.status(500).json({ error: "An error occurred while retrieving data" });
+  } finally {
+    if (db) await db.end(); // Asegúrate de cerrar la conexión siempre
   }
 };
 
 export const updateCliente = async (req, res) => {
+  let db;
   try {
-    const db = await connect();
-    await db.query("UPDATE clientes SET? WHERE idClientes =?", [
+    db = await connect();
+    await db.query("UPDATE clientes SET ? WHERE idClientes =?", [
       req.body,
       req.params.id,
     ]);
     res.json({ status: "ok" });
-    await db.end();
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "An error occurred while updating data" });
-    await db.end();
+  } finally {
+    if (db) await db.end(); // Asegúrate de cerrar la conexión siempre
   }
 };
 
 export const deleteCliente = async (req, res) => {
+  let db;
   try {
-    const db = await connect();
+    db = await connect();
     await db.query("DELETE FROM clientes WHERE idClientes =?", [req.params.id]);
     res.json({ status: "ok" });
-    await db.end();
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "An error occurred while deleting data" });
-    await db.end();
+  } finally {
+    if (db) await db.end(); // Asegúrate de cerrar la conexión siempre
   }
 };
