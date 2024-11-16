5a4d308523eeaf5ef14cdd857bc921d0cc9f8cbf
Samuel Reyna González <118919573+Meiuwu2001@users.noreply.github.com>
2024-11-15 22:54:04 -0600

    id equipo joins

--- src/controllers/joins.js
+++ src/controllers/joins.js
@@ -53,7 +53,7 @@ export const getEquipoUbicacion = async (req, res) => {
   const db = await connect();
   try {
     const [result] = await db.query(
-      "SELECT p.modelo, p.categoria, p.marca, e.numeroSerie, e.numeroEquipo, e.estatus, ub.nombre, ub.ciudad, ub.estado, ub.codigoPostal, ub.direccion FROM  productos p RIGHT JOIN equipos e ON p.idProductos = e.idProductos  LEFT JOIN ubicaciones ub ON e.idUbicaciones = ub.idUbicaciones"
+      "SELECT e.idEquipos, p.modelo, p.categoria, p.marca, e.numeroSerie, e.numeroEquipo, e.estatus, ub.nombre, ub.ciudad, ub.estado, ub.codigoPostal, ub.direccion FROM  productos p RIGHT JOIN equipos e ON p.idProductos = e.idProductos  LEFT JOIN ubicaciones ub ON e.idUbicaciones = ub.idUbicaciones"
     );
     res.json(result);
   } catch (error) {
@@ -99,7 +99,8 @@ export const getTecnicosActivosReportesPendientes = async (req, res) => {
   const db = await connect();
   try {
     const [result] = await db.query(
-      "SELECT CONCAT(t.Nombre, ' ', t.ApellidoPa) AS nombreTecnico, t.Telefono, r.folioReporte, r.estado, r.fechaCreacion, e.numeroEquipo, e.numeroSerie FROM tecnicos t LEFT JOIN reportes r ON t.idTecnicos = r.tecnicoAsignado LEFT JOIN equipos e ON r.idEquipos = e.idEquipos WHERE  t.Estatus = 'activo' AND r.estado = 'pendiente';");
+      "SELECT CONCAT(t.Nombre, ' ', t.ApellidoPa) AS nombreTecnico, t.Telefono, r.folioReporte, r.estado, r.fechaCreacion, e.numeroEquipo, e.numeroSerie FROM tecnicos t LEFT JOIN reportes r ON t.idTecnicos = r.tecnicoAsignado LEFT JOIN equipos e ON r.idEquipos = e.idEquipos WHERE  t.Estatus = 'activo' AND r.estado = 'pendiente';"
+    );
     res.json(result);
     await db.end();
   } catch (error) {
@@ -132,7 +133,7 @@ export const getTareasReporte = async (req, res) => {
   const db = await connect();
   try {
     const [result] = await db.query(
-      "SELECT tk.Titulo,    tk.Descripcion,tk.Observaciones,tk.estatus AS estatusTarea, r.folioReporte,r.fechaCreacion, r.fechaHoraActualizacion AS fechaModificacion,CONCAT(c.Nombre, ' ', c.ApellidoPa) AS nombreCliente,CONCAT(t.Nombre, ' ', t.ApellidoPa) AS tecnicoAsignado FROM  tareas tk INNER JOIN reportes r ON tk.idReportes = r.idReporte LEFT JOIN clientes c ON r.creadorReporte = c.idClientes LEFT JOIN tecnicos t ON r.tecnicoAsignado = t.idTecnicos WHERE r.idReporte = ?;",  
+      "SELECT tk.Titulo,    tk.Descripcion,tk.Observaciones,tk.estatus AS estatusTarea, r.folioReporte,r.fechaCreacion, r.fechaHoraActualizacion AS fechaModificacion,CONCAT(c.Nombre, ' ', c.ApellidoPa) AS nombreCliente,CONCAT(t.Nombre, ' ', t.ApellidoPa) AS tecnicoAsignado FROM  tareas tk INNER JOIN reportes r ON tk.idReportes = r.idReporte LEFT JOIN clientes c ON r.creadorReporte = c.idClientes LEFT JOIN tecnicos t ON r.tecnicoAsignado = t.idTecnicos WHERE r.idReporte = ?;",
       [req.params.id]
     );
     if (!result.length) {
@@ -165,4 +166,3 @@ export const getEquiposUbicacionById = async (req, res) => {
     await db.end();
   }
 };
-
