60819e0c3c5b83a8ab9867c9a184ac2da5e339fd
Samuel Reyna González <118919573+Meiuwu2001@users.noreply.github.com>
2024-11-23 18:08:50 -0600

    Coreccion de error en equipo controller

--- src/controllers/equipos.js
+++ src/controllers/equipos.js
@@ -1,35 +1,37 @@
 import connect from "../config/db.js"; // Importamos la conexión
 
 export const getEquipo = async (req, res) => {
+  const db = await connect();
+
   try {
-    const db = await connect();
     const [result] = await db.query("SELECT * FROM equipos;");
     res.json(result);
-    await db.end(); 
+    await db.end();
   } catch (error) {
     console.error(error);
     res.status(500).send("Server Error");
-    await db.end(); 
+    await db.end();
   }
 };
 
-
 export const createEquipo = async (req, res) => {
+  const db = await connect();
+
   try {
-    const db = await connect();
     await db.query("INSERT INTO equipos SET ?", [req.body]);
     res.json({ status: "ok" });
-    await db.end(); 
+    await db.end();
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "An error occurred while creating data" });
-    await db.end(); 
+    await db.end();
   }
 };
 
 export const getEquipoById = async (req, res) => {
+  const db = await connect();
+
   try {
-    const db = await connect();
     const [result] = await db.query(
       "SELECT * FROM equipos WHERE idEquipos =?",
       [req.params.id]
@@ -38,39 +40,41 @@ export const getEquipoById = async (req, res) => {
       return res.status(404).json({ error: "Equipo not found" });
     }
     res.json(result[0]);
-    await db.end(); 
+    await db.end();
   } catch (error) {
     console.error(error);
     res.status(500).send("Server Error");
-    await db.end(); 
+    await db.end();
   }
 };
 
 export const updateEquipo = async (req, res) => {
+  const db = await connect();
+
   try {
-    const db = await connect();
     await db.query("UPDATE equipos SET? WHERE idEquipos =?", [
       req.body,
       req.params.id,
     ]);
     res.json({ status: "ok" });
-    await db.end(); 
+    await db.end();
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "An error occurred while updating data" });
-    await db.end(); 
+    await db.end();
   }
 };
 
 export const deleteEquipo = async (req, res) => {
+  const db = await connect();
+
   try {
-    const db = await connect();
     await db.query("DELETE FROM equipos WHERE idEquipos =?", [req.params.id]);
     res.json({ status: "ok" });
-    await db.end(); 
+    await db.end();
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "An error occurred while deleting data" });
-    await db.end(); 
+    await db.end();
   }
 };
